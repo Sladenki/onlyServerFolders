7a90313b0227374fcb68b1302499bd7ea19be573
MarkSakhar <mark.sahar18@mail.ru>
2025-01-03 14:33:56 +0200

    add IP address

--- dist/main.js
+++ dist/main.js
@@ -14,6 +14,7 @@ async function bootstrap() {
             'ionic://localhost',
             'http://localhost',
             'https://graphon.up.railway.app',
+            'http://31.192.132.196:3000'
         ],
         credentials: true,
         exposedHeaders: 'set-cookie',
