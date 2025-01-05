f3a0a6fbe09dce263afcd596f67e684c48e9ee8b
MarkSakhar <mark.sahar18@mail.ru>
2025-01-06 00:14:53 +0200

    change redirect to web

--- dist/auth/auth.controller.js
+++ dist/auth/auth.controller.js
@@ -42,7 +42,7 @@ let AuthController = class AuthController {
             if (isCapacitor) {
                 const appScheme = 'com.mycompany.myapp://profile';
                 const redirectUrl = `${appScheme}?accessToken=${accessToken}`;
-                return res.json({ accessToken, redirectUrl });
+                return res.redirect(redirectUrl);
             }
             else {
                 res.redirect(`${process.env.CLIENT_URL}/profile?accessToken=${accessToken}`);
