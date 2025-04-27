9873e62a9406fcb2296a8db00d3a4d82ede781f6
MarkSakhar <mark.sahar18@mail.ru>
2025-04-27 12:14:56 +0200

    add HTTPS

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
+const fs = require("fs");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const httpsOptions = {
+        key: fs.readFileSync(process.env.KEY),
+        cert: fs.readFileSync(process.env.CERT),
+    };
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions });
     app.setGlobalPrefix('api');
     app.use(cookieParser());
     app.enableCors({
