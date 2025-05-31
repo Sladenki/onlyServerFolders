fa839dfaef541d7272467fc473928147617adbb4
MarkSakhar <mark.sahar18@mail.ru>
2025-05-31 15:47:37 +0200

    remove https

--- dist/main.js
+++ dist/main.js
@@ -3,13 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
-const fs = require("fs");
 async function bootstrap() {
-    const httpsOptions = {
-        key: fs.readFileSync(process.env.KEY),
-        cert: fs.readFileSync(process.env.CERT),
-    };
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix('api');
     app.use(cookieParser());
     app.enableCors({
